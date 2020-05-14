c9ad831773ee29b188019f8282b4e65cef8ad973
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2020-05-14 20:02:20 +0530

    adding base instance

--- src/components/API.js
+++ src/components/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `https://ancient-shelf-92540.herokuapp.com`
+  baseURL: 'https://ancient-shelf-92540.herokuapp.com',
+  headers: { "Content-type": "application/json" }
 });
