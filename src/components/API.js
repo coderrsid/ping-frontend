452a7c5d3d5a4fee67e9e083f01493d05bede348
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2020-05-14 23:18:23 +0530

    Added headers to axios

--- src/components/API.js
+++ src/components/API.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: "https://ancient-shelf-92540.herokuapp.com",
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+    "Access-Control-Allow-Origin": "*",
+  }
 });
 
 export default API;
